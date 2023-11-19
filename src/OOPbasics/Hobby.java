f228e20b35e5d814ceb360e10fd0f64783603064
Xeirah07 <suyatsarah519@gmail.com>
2023-11-19 09:04:32 +0800

    added OOPbasics package

---
+++ src/OOPbasics/Hobby.java
@@ -0,0 +1,22 @@
+package OOPbasics;
+
+
+class Hobby {
+    String name;
+    String hobbies;
+    
+    Hobby(String name, String hobbies) {
+        this.name = name;
+        this.hobbies = hobbies;
+  
+    }
+    String getName() {
+        return name;
+    }
+    String getHobbies() {
+        return hobbies;
+    }
+ 
+}
+    
+
---
+++ src/OOPbasics/Main.java
@@ -0,0 +1,12 @@
+package OOPbasics;
+
+public class Main {
+    public static void main(String [] args){
+        Hobby s= new Hobby("Sarah", "Reading");
+        
+        System.out.println("Name: " + s.getName());
+        System.out.println("Hobby: " + s.getHobbies());
+        
+    }
+    
+}
