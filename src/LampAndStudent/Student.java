9edaca459f793b52ac057f02786609d91fe978a0
Xeirah07 <suyatsarah519@gmail.com>
2023-11-19 11:56:09 +0800

    added code in Student class

--- src/LampAndStudent/Student.java
+++ src/LampAndStudent/Student.java
@@ -1,15 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package LampAndStudent;
 
-/**
- *
- * @author User
- */
-public class Student {
-    
+class Student {
+    String name;
+    int roll_no;
+}
+
+class Ans{
+    public static void main(String [] args){
+        Student s = new Student();
+        s.name = "Sarah";
+        s.roll_no = 20211254;
+        System.out.println("My name is " + s.name + " and my ID number is " + s.roll_no);
+    }
 }
