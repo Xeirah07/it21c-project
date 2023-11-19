8fb720ec8002761725cf0636c51c69689003da9d
Xeirah07 <suyatsarah519@gmail.com>
2023-11-20 07:07:50 +0800

    added code in Array class

--- src/SortingArray/Array.java
+++ src/SortingArray/Array.java
@@ -1,15 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package SortingArray;
 
-/**
- *
- * @author User
- */
 public class Array {
+    public static void main(String [] args){
+        int[] array = {9,7,8,15,3,2};
+        
+        int n = array.length;
+        for(int a = 0; a < n - 1; a++){
+            for (int b = 0; b < n - a - 1; b++){
+                if(array[b] > array[b+1]){
+                
+                int temp = array[b];
+                array[b] = array[b + 1];
+                array[b+1] = temp;
+            }
+        }
+        }
     
+    System.out.print("Ascending Order: ");
+        for(int value:array){
+            System.out.print(value + " ");
+}
+}
 }
