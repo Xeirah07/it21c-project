49e8ec15da8d26ea157605a33cb2ae6fe2d4d7cf
Xeirah07 <suyatsarah519@gmail.com>
2023-11-19 09:53:48 +0800

    added Student package

---
+++ src/Student/Main.java
@@ -0,0 +1,12 @@
+package Student;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Student s = new Student("Sarah Mae Suyat", "IT21C", "Trust the timing of your life");
+        
+        System.out.println("Name: " + s.getName());
+        System.out.println("Section: " + s.getSection());
+        System.out.println("Motto: " + s.getMotto());
+    }
+}
