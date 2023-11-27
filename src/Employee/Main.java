02981a413cd45eaaf631469dc857f22229b25275
Xeirah07 <suyatsarah519@gmail.com>
2023-11-27 23:28:27 +0800

    edited Employee

--- src/Employee/Main.java
+++ src/Employee/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 public static void main(String[] args) {
 		
-		Employee employee1 = new Employee ("Roberta Petrus", 50000, LocalDate.parse("2021-04-01"));
+	Employee employee1 = new Employee ("Roberta Petrus", 50000, LocalDate.parse("2021-04-01"));
         Employee employee2 = new Employee ("Loyd Blair", 70000, LocalDate.parse("2015-04-01"));
         Employee employee3 = new Employee ("Magdalena Artemon", 50000, LocalDate.parse("2011-01-15"));
         
